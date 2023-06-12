2fc2b1665d07376e6d41a238b0372f1c5b767396
deboschr <debospencher@gmail.com>
2023-06-13 06:35:43 +0700

    buat controller

---
+++ contriller.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const respose = require("./res");
+const connection = require("./koneksi");
+
+exports.index = function (req, res) {
+	respose.ok("Aplikasi REST API berjalan!");
+};
