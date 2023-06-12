71477df94f978ed2a68017c11168ad5de1e8e859
deboschr <debospencher@gmail.com>
2023-06-13 06:26:58 +0700

    menambahkan koneksi

---
+++ koneksi.js
@@ -0,0 +1,16 @@
+var mysqli = require("mysqli");
+
+// buat keneksi database
+const conn = mysqli.createConnection({
+	host: "localhost",
+	user: "root",
+	password: "",
+	database: "apprestapi",
+});
+
+conn.connect((err) => {
+	if (err) throw err;
+	console.log("MySQL Terkoneksi");
+});
+
+module.exports = conn;
