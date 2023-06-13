7b92d25dc4771b1b3b5750ad562ca3a46dae7641
deboschr <debospencher@gmail.com>
2023-06-14 02:14:24 +0700

    berhasil menampilkan daftar mahasiswa dari db

--- contriller.js
+++ contriller.js
@@ -6,3 +6,14 @@ const connection = require("./koneksi");
 exports.index = function (req, res) {
 	respose.ok("Aplikasi REST API berjalan!", res);
 };
+
+// menampilkan semua data mahasiswa
+exports.showAllMahasiswa = function (req, res) {
+	connection.query("SELECT * FROM mahasiswa", function (error, rows, fields) {
+		if (error) {
+			console.log(error);
+		} else {
+			respose.ok(rows, res);
+		}
+	});
+};
--- routes.js
+++ routes.js
@@ -1,7 +1,10 @@
 "use strict";
 
+const { json } = require("body-parser");
+
 module.exports = function (app) {
 	const myJSON = require("./contriller");
 
 	app.route("/").get(myJSON.index);
+	app.route("/tampil").get(myJSON.showAllMahasiswa);
 };
